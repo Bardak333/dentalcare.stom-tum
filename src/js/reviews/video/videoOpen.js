bf798b54f1614522bdd9304dbf0e43b3f2f21631
Bardak333 <adima20031@yandex.ru>
2025-11-08 22:58:56 +0500

    z

--- src/js/reviews/video/videoOpen.js
+++ src/js/reviews/video/videoOpen.js
@@ -3,7 +3,10 @@ const openVideoBtn = document.querySelector('.openVideo');
 function openVideo() {
 
     const videoContainer = document.querySelector('.main-item__case_view');
+    const videoIcon  = document.querySelector('.openVideo');
+
     videoContainer.classList.toggle('review_videoActive');
+    videoIcon.classList.toggle('closeVideo');
 }
 
 openVideoBtn.addEventListener('click', openVideo);
